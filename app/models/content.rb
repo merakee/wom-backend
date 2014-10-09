04fdcaf246cf856fe0ecca383210e343c20bf6fa
Freelogue <freelogue@users.noreply.github.com>
2014-10-09 12:08:08 -0700

    WV: added production branch with same AWS env

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,9 +1,8 @@
 class Content < ActiveRecord::Base  
-  #belongs_to :content_category
+  belongs_to :content_category
   belongs_to :user
   has_many  :user_response, dependent: :destroy
-  validates :user, presence: true
-  #validates :content_category, presence: true
+  validates :user, :content_category, presence: true
   validates :text, presence: true, length: { minimum: APIConstants::CONTENT::MIN_TEXT_LENGTH,
     maximum: APIConstants::CONTENT::MAX_TEXT_LENGTH}
   validates :text, uniqueness: { scope: [:user_id, :content_category_id],
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
-  #belongs_to :user_type
+  belongs_to :user_type
   has_many :content, dependent: :destroy
   has_many :user_response, dependent: :destroy
-  #validates :user_type, presence: true
+  validates :user_type, presence: true
   validates :password_confirmation, presence: true
   validates_format_of :email,:with => Devise.email_regexp
   validates_confirmation_of :password
